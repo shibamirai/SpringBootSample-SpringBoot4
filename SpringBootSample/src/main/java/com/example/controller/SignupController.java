b6c6455de4f1f088bd3e43d9489956d8a2927bd6
shibamirai <t_shiba@miraino-katachi.co.jp>
2025-11-28 16:56:19 +0900

    6.1.3 多言語化

--- SpringBootSample/src/main/java/com/example/controller/SignupController.java
+++ SpringBootSample/src/main/java/com/example/controller/SignupController.java
@@ -1,5 +1,6 @@
 package com.example.controller;
 
+import java.util.Locale;
 import java.util.Map;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -19,8 +20,8 @@ public class SignupController {
 	private UserApplicationService userApplicationService;
 	
 	@GetMapping("/signup")
-	public String getSignup(Model model) {
-		Map<String, Integer> genderMap = userApplicationService.getGenderMap();
+	public String getSignup(Model model, Locale locale) {
+		Map<String, Integer> genderMap = userApplicationService.getGenderMap(locale);
 		model.addAttribute("genderMap", genderMap);
 		
 		return "user/signup";
