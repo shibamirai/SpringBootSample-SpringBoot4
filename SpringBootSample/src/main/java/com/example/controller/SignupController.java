f77e8d3f7d6449c45e3e2558feb845bf4068a79b
shibamirai <t_shiba@miraino-katachi.co.jp>
2025-11-29 09:41:10 +0900

    6.3.3 バリデーションの実行順序の設定

--- SpringBootSample/src/main/java/com/example/controller/SignupController.java
+++ SpringBootSample/src/main/java/com/example/controller/SignupController.java
@@ -14,6 +14,7 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import com.example.application.service.UserApplicationService;
+import com.example.form.GroupOrder;
 import com.example.form.SignupForm;
 
 import lombok.extern.slf4j.Slf4j;
@@ -36,7 +37,7 @@ public class SignupController {
 	
 	@PostMapping("/signup")
 	public String postSignup(Model model, Locale locale,
-			@ModelAttribute @Validated SignupForm form,
+			@ModelAttribute @Validated(GroupOrder.class) SignupForm form,
 			BindingResult bindingResult) {
 		
 		if (bindingResult.hasErrors()) {
