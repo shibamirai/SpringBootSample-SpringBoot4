1280e459c9491fccc3002f6bc61a0a7809e4b33c
shibamirai <t_shiba@miraino-katachi.co.jp>
2025-11-29 09:31:17 +0900

    6.3.1 バリデーションの実装

--- SpringBootSample/src/main/java/com/example/controller/SignupController.java
+++ SpringBootSample/src/main/java/com/example/controller/SignupController.java
@@ -7,6 +7,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
+import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -34,7 +35,9 @@ public class SignupController {
 	}
 	
 	@PostMapping("/signup")
-	public String postSignup(Model model, Locale locale, @ModelAttribute SignupForm form, BindingResult bindingResult) {
+	public String postSignup(Model model, Locale locale,
+			@ModelAttribute @Validated SignupForm form,
+			BindingResult bindingResult) {
 		
 		if (bindingResult.hasErrors()) {
 			return getSignup(model, locale, form);
