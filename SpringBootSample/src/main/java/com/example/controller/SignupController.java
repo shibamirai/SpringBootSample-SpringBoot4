d4f89e8e994357d45157dceb4889212b0bcf4445
shibamirai <t_shiba@miraino-katachi.co.jp>
2025-11-29 09:21:31 +0900

    6.2.2 エラーメッセージの編集

--- SpringBootSample/src/main/java/com/example/controller/SignupController.java
+++ SpringBootSample/src/main/java/com/example/controller/SignupController.java
@@ -6,6 +6,7 @@ import java.util.Map;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -33,8 +34,12 @@ public class SignupController {
 	}
 	
 	@PostMapping("/signup")
-	public String postSignup(@ModelAttribute SignupForm form) {
+	public String postSignup(Model model, Locale locale, @ModelAttribute SignupForm form, BindingResult bindingResult) {
 		
+		if (bindingResult.hasErrors()) {
+			return getSignup(model, locale, form);
+		}
+
 		log.info(form.toString());
 		
 		return "redirect:/login";
