b3366e79c93e0d9599a8aaff4b2d137631595d0c
shibamirai <t_shiba@miraino-katachi.co.jp>
2025-12-08 15:31:15 +0900

    8.2.4 update・delete

--- SpringBootSample/src/main/java/com/example/repository/UserMapper.java
+++ SpringBootSample/src/main/java/com/example/repository/UserMapper.java
@@ -3,6 +3,7 @@ package com.example.repository;
 import java.util.List;
 
 import org.apache.ibatis.annotations.Mapper;
+import org.apache.ibatis.annotations.Param;
 
 import com.example.domain.user.model.MUser;
 
@@ -17,4 +18,13 @@ public interface UserMapper {
 	
 	/** ユーザー取得(1件) */
 	public MUser findOne(String userId);
+
+	/** ユーザー更新(1件) */
+	public void updateOne(
+			@Param("userId") String userId,
+			@Param("password") String password,
+			@Param("userName") String userName);
+
+	/** ユーザー削除(1件) */
+	public int deleteOne(@Param("userId") String userId);
 }
