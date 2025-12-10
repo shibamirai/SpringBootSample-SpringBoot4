d5d06eaeb2c28f78520166f8531fc86451c19754
shibamirai <t_shiba@miraino-katachi.co.jp>
2025-12-10 09:55:42 +0900

    13.3.3 テーブル結合(一対多)

---
+++ SpringBootSample/src/main/java/com/example/domain/user/model/SalaryKey.java
@@ -0,0 +1,12 @@
+package com.example.domain.user.model;
+
+import jakarta.persistence.Embeddable;
+
+import lombok.Data;
+
+@Embeddable
+@Data
+public class SalaryKey {
+	private String userId;
+	private String yearMonth;
+}
